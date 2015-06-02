44c04757ae6fad7c3ce76217bdb3302e96fdabde
Jason <jason@jcode.me>
2015-06-02 22:10:57 +0930

    Untested calc

--- stats/denna.py
+++ stats/denna.py
@@ -13,6 +13,19 @@ uses 0.39 x (words/unique words > 3 characters) + 11.8 x (syllables/words) - 15.
 import textifier
 
 class denna:
-    def __init__(self):
-        pass
+	import re
+	import textifier
 
+    tex = None
+
+    def __init__(self):
+        self.tex = textifier.textifier()
+
+    def process(self, text):
+        stats = self.tex.basic_stats(text)
+
+        uniqueWords = filter(lambda x: len(x) > 3, stats["words"]):
+        words_ps = 0.39 * (stats["words"] / float(uniqueWords))
+        syl_pw = 11.8 * (stats["syllables"]['total'] / float(stats["words"]))
+
+        return float("{0:.4f}".format(char_pw + words_ps - 21.43))
