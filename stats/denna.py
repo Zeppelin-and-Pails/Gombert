c93adf8f0dd0bf096cf2529881ec76895c1f04c5
Jason <jason@jcode.me>
2015-08-26 17:56:40 +0930

    Things are working

--- stats/denna.py
+++ stats/denna.py
@@ -13,6 +13,7 @@ An attempt at creating a readability index for song lyrics
 """
 import textifier
 import re
+import lzw
 
 class denna:
 
@@ -21,83 +22,12 @@ class denna:
     def __init__(self):
         self.tex = textifier.textifier()
 
-    def getPhrases(self, words, limit):
-        phrase = []
-        for word in words:
-            phrase.append(word)
-            if len(phrase) > limit:
-                phrase.remove(phrase[0])
-            if len(phrase) == limit:
-                yield tuple(phrase)
-
-    def getWords(self, text):
-        pattern = re.compile(r"[^\s]+")
-        non_alpha = re.compile(r"[^a-z]", re.IGNORECASE)
-        for match in pattern.finditer(text):
-            yield non_alpha.sub("", match.group()).lower()
-
-    def getSentences(self, text):
-        text = re.sub( '\n\n', '\n', text ).strip()
-        return text.splitlines()
-
-    def uniqueList(self, seq):
-        seen = set()
-        seen_add = seen.add
-        return [ x for x in seq if not (x in seen or seen_add(x))]
-
-    def cleanText(self, text):
-        non_alpha = re.compile(r"[^a-z\ \n]", re.IGNORECASE)
-        return non_alpha.sub("", text).lower()
-
-    def findLongestPhrases(self, uniqPhrases):
-        longestPhrases = []
-
-        for found in uniqPhrases:
-            f = filter(lambda x: found in x, uniqPhrases)
-            if len( f ) > 1:
-                maxf = max(f, key=len)
-                uniqPhrases = filter(lambda a: a != maxf, uniqPhrases)
-                longestPhrases.append(maxf)
-
-        return longestPhrases
-
     def determinRepetition(self, text):
-        scannedSentances = []
-        foundPhrases = []
-        longestPhrases = []
-        text = self.cleanText( text )
-        sentances = self.getSentences(text)
-        totalRepeats = 0
+        compressed = lzw.compress(text)
+        sbytes     = str.encode(text.encode('utf8'))
+        ratio      = float(len("".join(sbytes))) / float(len("".join(compressed)))
 
-        longestSentanceLength = len(max( sentances, key=len))
-
-        for sentance in sentances:
-            if sentance not in scannedSentances:
-                tcount = text.count(sentance)
-                if tcount > 1:
-                    scannedSentances.append(sentance)
-                    totalRepeats += tcount
-                    #print "Repeated sentance: '{}' repeated {} times".format(sentance, tcount)
-                    sentances = filter(lambda a: a != sentance, sentances)
-
-        for sentanceLength in range(3, longestSentanceLength):
-            for sentance in sentances:
-                results = list(self.getPhrases(self.getWords(sentance), sentanceLength ))
-                if results:
-                    for phrase in results:
-                        foundPhrases.append( ' '.join( phrase ) )
-
-        uniqPhrases = self.uniqueList(foundPhrases)
-
-        longestPhrases = self.findLongestPhrases(uniqPhrases)
-
-        for phrase in longestPhrases:
-            tcount = text.count(phrase)
-            if tcount > 1:
-                #print "Repeated phrase: '{}' repeated {} times".format(phrase, tcount)
-                totalRepeats += tcount
-
-        return totalRepeats
+        return ratio
 
     def findPolysyllables(self, syllables):
         polysyllables = 0
@@ -107,20 +37,25 @@ class denna:
 
         return polysyllables
 
+    def cleanText(self, text):
+        non_alpha = re.compile(r"[^a-z\ \n]", re.IGNORECASE)
+        return non_alpha.sub("", text).lower()
+
     def determinSongLength(self, text):
         return len(self.cleanText( text ))
 
     def process(self, text):
-        stats           = self.tex.basic_stats(text)
-        syl             = self.tex.syllables(text)
-        try:
-            uniqueWords     = filter(lambda x: len(x) > 3, self.tex.unique_words(text))
-            polysyllables   = float(self.findPolysyllables(syl))
-            repetition      = float(self.determinRepetition(text))
-            songLength      = float(self.determinSongLength(text))
-            syllables       = float(syl['total'])
+        stats               = self.tex.basic_stats(text)
+        syl                 = self.tex.syllables(text)
 
-            d = ((( float( len(uniqueWords) ) / float( stats["words"] ) ) / ( syllables / (1 + polysyllables) )) * (12.0 + ( repetition / songLength) )) * 100
+        try:
+            syllables       = float(syl['total'])
+            repetition      = float(self.determinRepetition(text))
+            polysyllables   = float(self.findPolysyllables(syl))
+            uniqueWords     = filter(lambda x: len(x) > 3, self.tex.unique_words(text))
+            songLength      = float(self.determinSongLength(text))
+
+            d = ((( float( len(uniqueWords) ) / float( stats["words"] ) ) / ( syllables / (1 + polysyllables) )) / (repetition)) * 100
         except ValueError:
             d = 0
         except:
